d2f1a715cd80d9fc712dbb991cb53bc8d1e0500a
xbasset <xavier.basset@hoomano.com>
2025-01-03 12:55:28 +0100

    (rename) mc do example

--- roles/🍔_order_mac_donalds/data.py
+++ roles/🍔_order_mac_donalds/data.py
@@ -1,6 +1,6 @@
 # load mc_do_data.json file as "menu" variable
 import json
 
-with open("roles/🎭_actor/mc_do_data.json") as f:
+with open("roles/🍔_order_mac_donalds/mc_do_data.json") as f:
     menu = json.load(f)
     f.close()
